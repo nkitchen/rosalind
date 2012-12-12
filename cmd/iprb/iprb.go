251414561176ca138f63fee975de2641385fe949
Nathan Kitchen <nkitchen@aristanetworks.com>
2012-12-12 15:45:48 -0800

    Removed Monte-Carlo code.

--- cmd/iprb/iprb.go
+++ cmd/iprb/iprb.go
@@ -52,23 +52,4 @@ func main() {
 	pDom += n / all * (k + 0.5 * m) / (all - 1)
 	
 	fmt.Println(pDom)
-
-	func (k, m, n int) {
-		hits := 0.0
-		trials := 0.0
-		for i := 0; i < 10000000; i++ {
-			r1 := rand.Intn(k + m + n)
-			r2 := r1
-			for r2 == r1 {
-				r2 = rand.Intn(k + m + n)
-			}
-			d1 := r1 < k || (r1 < k + m && rand.Intn(2) == 0)
-			d2 := r2 < k || (r2 < k + m && rand.Intn(2) == 0)
-			if d1 || d2 {
-				hits += 1
-			}
-			trials += 1
-		}
-		fmt.Println(hits / trials)
-	}(int(k), int(m), int(n))
 } 
