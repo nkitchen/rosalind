991bd6f89cb487f7230d0d334ce704dc369f7bbc
Nathan Kitchen <nkitchen@aristanetworks.com>
2013-01-30 13:10:13 -0800

    Rewrote character-table construction to use big.Ints.

--- cmd/ctbl/ctbl.go
+++ cmd/ctbl/ctbl.go
@@ -2,10 +2,16 @@ package main
 
 import "bufio"
 import "fmt"
+import "math/big"
 import "os"
 import "rosalind/tree"
 import "sort"
 import "strings"
+
+type CharacterTable struct {
+	Data []*big.Int
+	NumTaxa int
+}
 
 func main() {
 	br := bufio.NewReader(os.Stdin)
@@ -17,18 +23,10 @@ func main() {
 		os.Exit(1)
 	}
 
-	taxa := findTaxa(tree.Edge{t,0}, nil)
-	sort.StringSlice(taxa).Sort()
-
-	taxaInv := map[string]int{}
-	for i, taxon := range taxa {
-		taxaInv[taxon] = i
-	}
-
-    ctab, _ := charTable(tree.Edge{t,0}, taxaInv, nil)
-	for _, array := range ctab {
-		for _, c := range array {
-			fmt.Print(c)
+	ctab := charTable(t)
+	for _, array := range ctab.Data {
+		for i := 0; i < ctab.NumTaxa; i++ {
+			fmt.Print(array.Bit(i))
 		}
 		fmt.Println()
 	}
@@ -45,31 +43,39 @@ func findTaxa(t tree.Edge, taxa []string) []string {
 }
 
 // charTable returns the character table and the set of taxa for the subtree.
-func charTable(t tree.Edge, taxaInv map[string]int, table [][]int) ([][]int, map[string]bool) {
-	s := make(map[string]bool)
+// This version is optimized with the assumption that taxa are only
+// at the leaves and that they are distinct.
+func charTable(t *tree.Node) CharacterTable {
+	taxa := findTaxa(tree.Edge{t,0}, nil)
+	sort.StringSlice(taxa).Sort()
+
+	taxaInv := map[string]int{}
+	for i, taxon := range taxa {
+		taxaInv[taxon] = i
+	}
+
+	ctab, _ := subtreeCharTable(tree.Edge{t, 0}, taxaInv, CharacterTable{})
+	// The last array just includes all the taxa.
+	ctab.Data = ctab.Data[:len(ctab.Data)-1]
+	ctab.NumTaxa = len(taxaInv)
+	return ctab
+}
+
+func subtreeCharTable(t tree.Edge, taxaInv map[string]int, table CharacterTable) (CharacterTable, *big.Int) {
+	s := big.NewInt(0)
 
 	if t.Label != "" {
-		s[t.Label] = true
+		s.SetInt64(1)
+		s.Lsh(s, uint(taxaInv[t.Label]))
 	}
 
-	added := false
 	for _, u := range t.Children {
-		var r map[string] bool
-		table, r = charTable(u, taxaInv, table)
-		for label := range r {
-			if !s[label] {
-				added = true
-			}
-			s[label] = true
-		}
+		var r *big.Int
+		table, r = subtreeCharTable(u, taxaInv, table)
+		s.Or(s, r)
 	}
-
-	if 1 < len(s) && len(s) < len(taxaInv) - 1 && added {
-		a := make([]int, len(taxaInv))
-		for label := range s {
-			a[taxaInv[label]] = 1
-		}
-		table = append(table, a)
+	if len(t.Children) > 0 {
+		table.Data = append(table.Data, s)
 	}
 
 	return table, s
